dac30a08ff17c7ecd57807adcaa0e768c9a6752a
invx <110623554+invxz@users.noreply.github.com>
2023-06-07 11:02:03 -0300

    Add files via upload

---
+++ desafiomaneirao/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace desafiomaneirao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double nota = 0.00;
+
+            Console.WriteLine("Digite seu name INTENSO: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("digite sua notaaa:");
+            nota = double.Parse(Console.ReadLine());
+
+            if (nota <= 5.9)
+            {
+                Console.WriteLine("Oi " + nome + " você esta de recuperação.");
+            }
+
+            else if(nota >=6)
+            {
+                Console.WriteLine("Oi " + nome + " você passou!");
+            }
+
+        }
+    }
+}
